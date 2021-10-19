e5f845a428316d1dd7797794e5c5d4466330e7a6
Daniil-Shu <daniilshurikov@yandex.ru>
2021-10-19 21:07:15 +0300

    Написана программа к заданию 1-1
    1) DS_Store
    2) main.cpp
    3) project.pbxproj
    4) IDEWorkspaceChecks.plist

---
+++ 1-1/1-1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double GetA(const double x, const double y, const double z);
+double GetB(const double x, const double y, const double z);
+
+int main()
+{
+    const auto x = 0.2;
+    const auto y = 0.004;
+    const auto z = 1.1;
+
+ //   const auto a = GetA(x, y, z);
+    const auto a = (pow(sin(pow((pow(x, 2) + z) , 2)) , 3)) - sqrt(x/y);
+    const auto b = GetB(x, y, z);
+
+    std::cout << "x = " << x << ", y = " << y << ", z = " << z << '\n';
+    std::cout << "a = " << a << ", b = " << b << endl;
+    return 0;
+
+}
+
+double GetA(const double x, const double y, const double z)
+{
+    return (pow(sin(pow((pow(x, 2)+z),2)),3) ) - sqrt(x/y);
+    return pow(sin(pow(x,2)),3);
+}
+
+double GetB(const double x, const double y, const double z)
+{
+    return pow(x, 2) / z + cos(pow((x+y), 3));
+}
