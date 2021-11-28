e6b10a7909dc2dc27f372d4c6740d42b747aaf4e
Daniil-Shu <daniilshurikov@yandex.ru>
2021-11-28 21:34:47 +0300

    Переделал задание 4-1

--- 4-1/main.cpp
+++ 4-1/main.cpp
@@ -1,206 +1,200 @@
 #include <iostream>
-#include <cmath>
 #include <random>
-#include <string>
-#include <sstream>
-
-
 using namespace std;
 
-/**
-*\brief Нахождение минимального по значению элемента
-*\param array Массив
-*\param size Размер массива
-**/
-size_t FindMin(const int* array, const size_t size);
+/*
+* \brief - заполнение массива пользователем
+* \param arr массив
+* \param size размер массива
+*/
+void userInput(int* arr, const size_t size);
 
-/**
-*\brief Нахождение среднего арифметического среди элементов массива
-*\param array Массив
-*\param size Размер массива
-**/
-int FindAverage(const int* array, const size_t size);
+/*
+* \brief заполнение массива случайными числами
+* \param arr массив
+* \param size размер массива
+*/
+void randomDigits(int* arr, const size_t size);
 
-/**
-*\brief Замена минимального элемента массива на среднее арифметическое
-*\param array Массив
-*\param size Размер массива
-**/
-void  ReplaceMinToAverage(int* array, const size_t size);
+/*
+* \brief вывод массива
+* \param arr массив
+* \param size размер массива
+*/
+void output(const int* arr, const size_t size, const string& message);
+/*
+* \brief нахождение минимального элемента массива
+* \param arr массив
+* \param size размер массива
+* \return минимальный элемент массива
+*/
+int findMin(int* arr, const size_t size);
 
-/**
-*\brief Вывод индексов элементов, значение которых больше предыдущих
-*\param array Массив
-*\param size Размер массива
-**/
-void ShowIndexes(int* array, const size_t size);
-/**
-*\brief Проверка на существование двух пар элементов с одинаковыми знаками
-*\param array Массив
-*\param size Размер массива
-**/
-bool ElementExist(int* array, const size_t size);
+/*
+* \brief - замена среднего элемента массива на минимальный
+* \param arr массив
+* \param size размер массива
+* \param min минимальный элемент массива
+*/
+void replacingTheMinimumElementWithTheMiddleOne(int* arr, const size_t size, const int min);
 
-/**
+/*
+* \brief вывод на экран индексов элементов больших чем предыдущий элемент
+* \param arr массив
+* \param size размер массива
+*/
+void showIndexes(int* array, const size_t size);
+
+/*
+* \brief Проверка на существование двух пар элементов с одинаковыми знаками
+* \param array Массив
+* \param size Размер массива
+* \return true если пара существует
+*/
+bool elementExist(int* array, const size_t size);
+
+/*
 * \brief Выбор заполнеия массива
-**/
-enum class FillType
+*/
+enum class path
 {
-    randomize = 1,
-    manual = 2
+	Manuall,
+	Random
 };
 
-/**
-* \brief Заполнение массива случайными числами
-* \array Массив
-* \size Размер массива
-* \param min Минимально возможное число
-* \param max Максимально возможное число
-**/
-void randomizeArray(int* array, const size_t size, const int min, const int max);
-
-/**
-* \brief Ввод массива с клавиатуры
-* \param array Массив
-* \param size Размер массива
-**/
-void manualInput(int* array, const size_t size);
-
-/**
-* \brief Конвертация массива строку вида {element_1, element_2, ... ,element_n}
-* \param array Массив
-* \param size Размер массива
-* \param message Сообщение, которое будет выведено перед массивом
-**/
-string arrayToString(int* array, const size_t size, const string& message);
-
-int main() {
-
-
-
-    const int min = -20, max = 40;
-
-    cout << "Введите размер массива\n";
-    size_t size;
-    cin >> size;
-    int* array = new int[size];
-
-    cout << "Выберите способ заполнения массива:\n" << "Случайные элементы - 1\n" << "Ручной ввод - 2\n";
-
-    int choice;
-    cin >> choice;
-
-    const auto fillType = static_cast<FillType>(choice);
-
-    switch (fillType)
-    {
-    case FillType::randomize:
-    {
-        randomizeArray(array, size, min, max);
-        cout << arrayToString(array, size, "Ваш массив = ") << "\n";
-        break;
-    }
-    case FillType::manual:
-    {
-        manualInput(array, size);
-        cout << arrayToString(array, size, "Ваш массив = ") << "\n";
-        break;
-    }
-    default:
-        break;
-    }
-
-    cout << "Индексы элементов, значения которых больше предыдущих\n";
-
-    ShowIndexes(array, size);
-
-    if (ElementExist(array, size))
-        cout << "Существует ";
-    else
-        cout << "Не существует ";
-    cout << "две пары соседних элементов с одинаковыми знаками\n";
-
-    ReplaceMinToAverage(array, size);
-    cout << arrayToString(array, size, "Измененный массив = ") << '\n';
-
-    delete[] array;
-}
-
-void randomizeArray(int* array, const size_t size, const int min, const int max)
+/*
+* \brief Точка входа в программу
+* \return Код ошибки(0 - успех)
+*/
+int main()
 {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    const uniform_int_distribution<> uniformIntDistribution(min, max);
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите количество элементов массива - ";
+	size_t size;
+	cin >> size;
+	int* arr = new int[size];
 
-    for (size_t i = 0; i < size; ++i)
-    array[i] = uniformintdistribution(gen);
+	cout << "Как хотите заполнить массив?\n"
+		<< static_cast<int>(path::Manuall) << " - заполнение массива с клавиатуры,\n"
+		<< static_cast<int>(path::Random) << " - заполнение массива случайными числами.\n"
+		<< "Ваш выбор: ";
+	int choice;
+	cin >> choice;
+	const auto chosen = static_cast<path>(choice);
+	switch (chosen) 
+	{
+	  case path::Random:
+	  {
+		randomDigits(arr, size);
+		break;
+	  }
+	  case path::Manuall:
+	  {
+		userInput(arr, size);
+		break;
+	  }
+	  default:
+	  {
+		cout << "Некоректный ввод.";
+		return 0;
+	  }
+	}
 
+	output(arr, size, "Ваш массив: ");
+
+	int min = findMin(arr, size);
+	cout << "\nМинимальный элемент массива = " << min << "\n";
+
+	if (size % 2 != 0)
+	{
+		replacingTheMinimumElementWithTheMiddleOne(arr, size, min);
+		output(arr, size, "Массив после замены среднего элемента на минимальный:");
+	}
+	else
+	{
+		cout << "Массив содержит четное число элементов.\n";
+		output(arr, size, "Ваш массив: ");
+	}
+
+	cout << "\nИндексы элементов, значение которых больше предыдущего: \n";
+	showIndexes(arr, size);
+
+	if (elementExist(arr, size))
+		cout << "Существуют пары с одинаковыми знаками";
+	else
+		cout << "Не существуют пары с одинаковыми знаками";
+
+	if (arr != nullptr)
+	{
+		delete[] arr;
+		arr = nullptr;
+	}
+	return (0);
 }
 
-void manualInput(int* array, const size_t size)
+void randomDigits(int* arr, const size_t size)
 {
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << "array[" << i << "] = ";
-        cin >> array[i];
-    }
+	const int UP_BOUND = 40;
+	const int LOW_BOUND = -20;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	const std::uniform_int_distribution<> uniformIntDistribution(LOW_BOUND, UP_BOUND);
+
+	for (size_t index = 0; index < size; index++)
+		arr[index] = uniformIntDistribution(gen);
 }
 
-string arrayToString(int* array, const size_t size, const string& message = "")
+void userInput(int* arr, const size_t size)
 {
-    std::stringstream buffer;
-    buffer << message << "{" << array[0];
-
-    for (size_t i = 1; i < size; ++i)
-    {
-        buffer << ", " << array[i];
-    }
-
-    buffer << "}";
-
-    return buffer.str();
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << "array[" << i << "] = ";
+		cin >> arr[i];
+	}
 }
 
-size_t FindMin(int* array, size_t size) {
-    size_t minIndex = 0;
-
-    for (size_t i = 1; i < size; i++)
-    {
-        if (array[minIndex] > array[i])
-            minIndex = i;
-    }
-
-    return minIndex;
-}
-
-int FindAverage(int* array, size_t size) {
-    int average = 0;
-    for (size_t i = 1; i < size; i++) {
-        average += array[i];
-    }
-    return average / size;
-}
-
-void  ReplaceMinToAverage(int* array, size_t size)
+void output(const int* arr, const size_t size, const string& message)
 {
-    array[FindMin(array, size)] = FindAverage(array, size);
+	cout << message;
+	size_t counter = 0;
+	cout << "[ ";
+	while (counter < size)
+		cout << arr[counter++] << " ";
+	cout << "]";
 }
 
-void ShowIndexes(int* array, size_t size) {
-    if (size <= 2)
-        return;
+int findMin(int* arr, const size_t size)
+{
+	int min = 2147483647;
+	for (size_t i = 0; i < size; i++)
+		if (arr[i] < min)
+			min = arr[i];
+	return (min);
+}
 
-    for (size_t i = 1; i < size - 1; i++)
-        if (array[i] < array[i + 1])
-            cout << "index = " << i + 1 << '\n';
+void replacingTheMinimumElementWithTheMiddleOne(int* arr, const size_t size, const int min)
+{
+	arr[size / 2] = min;
+}
+
+void showIndexes(int* array, const size_t size)
+{
+	if (size <= 2)
+		return;
+
+	for (size_t i = 1; i < size - 1; i++)
+		if (array[i] < array[i + 1])
+			cout << "индекс = " << i + 1 << '\n';
 
 }
 
-bool ElementExist(int* array, size_t size) {
-    int k = 0;
+bool elementExist(int* array, size_t size)
+{
+	int k = 0;
 
-    for (size_t i = 0; i < size; i++)
-        if (array[i] * array[i + 1] > 0)
-            k++;
-    return k >= 2;
+	for (size_t i = 0; i < size; i++)
+		if (array[i] * array[i + 1] > 0)
+			k++;
+	return k >= 2;
 }
